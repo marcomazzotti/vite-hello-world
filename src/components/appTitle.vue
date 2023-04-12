622325b9123869f230a55b0c98f7334c8e7e8542
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-04-12 16:26:32 +0200

    Add: completed exercise

---
+++ src/components/appTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+  name: "appTitle",
+  data(){
+    return{
+      title: "La mia prima app con Vite!"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style></style>
